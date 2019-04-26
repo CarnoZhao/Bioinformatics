176f09a8d21eb7f17d2e083a19ec8824989aae6f
CarnoZhao <273806108@qq.com>
2019-04-26 10:49:34 -0400

    finishCHP1

--- Cookbook/Chapter1/Read_Base_Count_Scatter.R
+++ Cookbook/Chapter1/Read_Base_Count_Scatter.R
@@ -1,14 +1,15 @@
 library(ggplot2)
 
 pdf('Read_Base_Count_Scatter.pdf')
-d = read.table('../CookBookData/sequence.index',
+d = read.table('../../../DataLists/CookbookData/sequence.index',
 			   header = TRUE,
 			   sep = '\t',
-			   fill = TRUE)
+			   fill = TRUE,
+			   stringsAsFactors = FALSE)
 d$BASE_COUNT = sapply(d$BASE_COUNT, as.integer)
 d$READ_COUNT = sapply(d$READ_COUNT, as.integer)
 
 d = d[d$POPULATION %in% c('YRI', 'CEU') & d$BASE_COUNT < 2E9 & d$READ_COUNT < 3E7, c('POPULATION', 'BASE_COUNT', 'READ_COUNT', 'ANALYSIS_GROUP')]
-graph = ggplot(d, aes(x = BASE_COUNT, y = READ_COUNT, shape = POPULATION, color = ANALYSIS_GROUP)) + geom_point() + xlim(c(0, 2E9)) + ylim(c(0, 3E7))
+graph = ggplot(d, aes(x = BASE_COUNT, y = READ_COUNT, shape = POPULATION, color = ANALYSIS_GROUP)) + geom_point()
 plot(graph)
 dev.off()

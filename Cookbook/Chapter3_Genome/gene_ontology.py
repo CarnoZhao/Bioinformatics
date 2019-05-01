225c6fc9f37bd6e995dc3a64d533a7cceb458c23
CarnoZhao <273806108@qq.com>
2019-05-01 10:57:45 -0400

    ontology_some

--- Cookbook/Chapter3_Genome/gene_ontology.py
+++ Cookbook/Chapter3_Genome/gene_ontology.py
@@ -20,6 +20,9 @@ class Gene_Ontology(Ensembl_Rest_Api):
 			print('%s\n' % details['definition'])
 
 	def detail_about_go_id(self, go_id = 'GO:0000016'):
+		'''
+		get the detail information of one go id node, including its parent
+		'''
 		data = self.do_request('ontology/id', go_id)
 		for k, v in data.items():
 			if k == 'parents':
@@ -32,6 +35,9 @@ class Gene_Ontology(Ensembl_Rest_Api):
 		print(parent_id, len(parent_data['children']))
 
 	def get_all_ancestors(self, go_id = 'GO:0000016'):
+		'''
+		get all the ancestors of a certain go id, stop until 3 roots (Molecular funtion, cell component, biological processes)
+		'''
 		refs = self.do_request('ontology/ancestors/chart', go_id)
 		for go, entry in refs.items():
 			print(go)
@@ -39,6 +45,9 @@ class Gene_Ontology(Ensembl_Rest_Api):
 			print('%s: %s' % (term['name'], term['definition']))
 			is_a = entry.get('is_a', [])
 			print('\t is a: %s\n' % ', '.join([x['accession'] for x in is_a]))
+
+	def get_upper(self, go_id):
+		
 
 if __name__ == '__main__':
 	go = Gene_Ontology()
